fd9a7f6e9010881b8c7e324db7105902c32cbc6a
Ruhul Amin <mruhul@gmail.com>
2016-06-13 22:25:13 +1000

    initial commit

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace ConsoleApplication
+namespace BookStore.Web
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+
             Console.WriteLine("Hello World!!");
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=index}/{id?}");
+            });
+
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync("Hello world from asp.net");
+            });
+        }
+    }
+    
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Content("Hello world this is attribute routing");
         }
     }
 }
