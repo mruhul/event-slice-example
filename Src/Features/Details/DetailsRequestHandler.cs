0f68dd78046e87a1c394005041932137617fc7e8
Ruhul Amin <mruhul@gmail.com>
2016-07-25 20:36:52 +1000

    move details dto to its new file

--- Src/Features/Details/DetailsRequestHandler.cs
+++ Src/Features/Details/DetailsRequestHandler.cs
@@ -75,18 +75,4 @@ namespace BookWorm.Web.Features.Details
             };
         }
     }
-
-    public class BookDetailsDto
-    {
-        public string Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Image { get; set; }
-        public decimal Price { get; set; }
-        public string Description { get; set; }
-
-        public string ISBN { get; set; }
-        public string Category { get; set; }
-
-    }
 }
